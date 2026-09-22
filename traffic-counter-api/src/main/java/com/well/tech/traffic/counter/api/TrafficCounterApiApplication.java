0a007e5b2d166d4afd403f76fc2ccb28544baeea
Wellington Silva <wellingtonsc88@gmail.com>
2026-09-22 13:01:36 -0300

    feat: initial microservice module configuration

---
+++ traffic-counter-api/src/main/java/com/well/tech/traffic/counter/api/TrafficCounterApiApplication.java
@@ -0,0 +1,13 @@
+package com.well.tech.traffic.counter.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficCounterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficCounterApiApplication.class, args);
+    }
+
+}
