4662d92843519b440a4a361808e247028c8c3542
Wellington <133610786+wellsc88@users.noreply.github.com>
2026-09-23 09:37:36 -0300

    traffic-counter-api (feat): add REST controllers (#5)

---
+++ traffic-counter-api/src/main/java/com/well/tech/traffic/counter/api/config/ApiVersion.java
@@ -0,0 +1,10 @@
+package com.well.tech.traffic.counter.api.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiVersion {
+    public static final String API_BASE_PATH = "/api";
+    public static final String API_VERSION = "v1";
+}
---
+++ traffic-counter-api/src/main/java/com/well/tech/traffic/counter/api/controller/TrafficDeviceController.java
@@ -0,0 +1,61 @@
+package com.well.tech.traffic.counter.api.controller;
+
+import com.well.tech.traffic.counter.api.dto.request.TrafficDeviceRequest;
+import com.well.tech.traffic.counter.api.dto.response.TrafficDeviceResponse;
+import com.well.tech.traffic.counter.api.service.TrafficDeviceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+import static com.well.tech.traffic.counter.api.config.ApiVersion.API_BASE_PATH;
+import static com.well.tech.traffic.counter.api.config.ApiVersion.API_VERSION;
+
+@RestController
+@RequestMapping(API_BASE_PATH + "/" + API_VERSION + "/traffic-devices")
+@RequiredArgsConstructor
+public class TrafficDeviceController {
+
+    private final TrafficDeviceService service;
+
+    @GetMapping
+    public ResponseEntity<List<TrafficDeviceResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TrafficDeviceResponse> findById(
+            @PathVariable UUID id) {
+
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TrafficDeviceResponse> save(
+            @RequestBody TrafficDeviceRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(service.save(request));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TrafficDeviceResponse> update(
+            @PathVariable UUID id,
+            @RequestBody TrafficDeviceRequest request) {
+
+        return ResponseEntity.ok(service.update(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(
+            @PathVariable UUID id) {
+
+        service.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
